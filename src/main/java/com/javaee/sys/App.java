fd29f7050d51c7d63d96390bc749145972f78808
Mercy Bore <mercycherotich757@gmail.com>
2022-09-25 23:22:59 +0300

    java with maven

---
+++ src/main/java/com/javaee/sys/App.java
@@ -0,0 +1,14 @@
+package com.javaee.sys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Java EE!" );
+        System.out.println("Learn with Maven.");
+    }
+}
